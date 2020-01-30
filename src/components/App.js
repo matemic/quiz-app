46e540e06d0d2dcbc01875c8eaee947fe68933ad
= <=>
2020-01-30 05:57:29 +0100

    Add App.module.css and update import in index.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App = () => {
+	return (
+		<div className={styles.App}>
+			<header className="App-header" />
+		</div>
+	);
+};
+
+export default App;
