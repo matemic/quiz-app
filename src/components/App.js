55e083bc11acf45568e560027b7e5c38e2f2c174
= <=>
2020-01-30 22:09:03 +0100

    Add React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import styles from './App.module.css';
 
 const App = () => {
 	return (
 		<div className={styles.App}>
-			<header className="App-header" />
+			<Router />
 		</div>
 	);
 };
