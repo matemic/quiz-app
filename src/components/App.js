517b56477151d94d16c316669c2c3ba1e5543820
= <=>
2020-02-03 22:26:57 +0100

    Add Material UI theme Provider to change global styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import styles from './App.module.css';
+import Header from './Header/Header';
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: '#009688'
+		}
+	}
+});
 
 const App = () => {
 	return (
 		<div className={styles.App}>
-			<Router />
+			<ThemeProvider theme={theme}>
+				<Router>
+					<Header />
+				</Router>
+			</ThemeProvider>
 		</div>
 	);
 };
