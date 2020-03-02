306b306d7960f76dc007757d5fc88f3adf952f8a
matemic <mnamtar@gmail.com>
2020-03-02 17:51:26 +0100

    Add routes for Home and QuizForm components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Header from './Header/Header';
+import QuizForm from './Quiz/QuizForm';
+import Home from './Home/Home';
 import './App.module.css';
 const theme = createMuiTheme({
 	palette: {
@@ -16,6 +18,14 @@ const App = () => {
 		<ThemeProvider theme={theme}>
 			<Router>
 				<Header />
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/quiz-form">
+						<QuizForm />
+					</Route>
+				</Switch>
 			</Router>
 		</ThemeProvider>
 	);
