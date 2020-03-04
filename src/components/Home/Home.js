52a367c6232168a04e001aa0898642a08de282b1
matemic <mnamtar@gmail.com>
2020-03-04 14:44:22 +0100

    Add basic HTML content and the styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
 	Home: {
-		marginTop: '5rem'
+		marginTop: '7rem',
+		display: 'flex',
+		justifyContent: 'center'
+	},
+	HomeBox: {
+		width: '80%',
+		padding: '2rem'
+	},
+	Heading: {
+		textAlign: 'center',
+		margin: '1rem 0 2rem 0'
 	}
 }));
 
@@ -13,8 +27,35 @@ const Home = () => {
 	const classes = useStyles();
 	return (
 		<div className={classes.Home}>
-			<Paper square>
-				<Typography variant="h3">Wanna try?</Typography>
+			<Paper className={classes.HomeBox} square>
+				<Typography className={classes.Heading} variant="h3">
+					About Quiz App
+				</Typography>
+				<Grid container spacing={3}>
+					<Grid item md={6}>
+						<Typography>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta eius minima consequatur
+							debitis, reiciendis laboriosam accusamus illum adipisci facere cupiditate voluptatem quaerat
+							odit assumenda, cumque odio ducimus obcaecati, ab ipsa? Cupiditate vel ducimus harum illum
+							consectetur, autem totam alias laboriosam qui molestias tempore! Ab dolor sit quas vel
+							illum! Unde ad expedita ullam ea cumque repudiandae, ipsam odit veniam reprehenderit?
+							Consectetur sunt officia molestiae enim labore perspiciatis quidem fuga corporis sit culpa
+							quam neque unde laborum eius nisi, ullam, animi, magni dolorum placeat fugiat molestias
+							iste. Molestiae distinctio optio esse!
+						</Typography>
+					</Grid>
+					<Grid item>
+						<Divider variant="middle" orientation="vertical" />
+					</Grid>
+					<Grid item md={4}>
+						<Typography className={classes.Heading} variant="h4">
+							Ready for a challenge?
+						</Typography>
+						<Button component={Link} to="/quiz-form" variant="contained">
+							Go to Quiz
+						</Button>
+					</Grid>
+				</Grid>
 			</Paper>
 		</div>
 	);
