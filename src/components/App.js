c9aa39e01f8fa7b869eacb5e7930bb3bab21c7be
= <=>
2020-03-10 22:36:02 +0100

    Add global main and styles when switching between different views

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Header from './Header/Header';
+import Paper from '@material-ui/core/Paper';
 import QuizForm from './Quiz/QuizForm';
 import Home from './Home/Home';
 import './App.module.css';
+
 const theme = createMuiTheme({
 	palette: {
 		primary: {
@@ -13,19 +16,38 @@ const theme = createMuiTheme({
 	}
 });
 
+const useStyles = makeStyles(() => ({
+	rootMain: {
+		marginTop: '7rem',
+		display: 'flex',
+		justifyContent: 'center'
+	},
+	container: {
+		width: '80%',
+		padding: '2rem'
+	}
+}));
+
 const App = () => {
+	const classes = useStyles();
 	return (
 		<ThemeProvider theme={theme}>
 			<Router>
 				<Header />
-				<Switch>
-					<Route exact path="/">
-						<Home />
-					</Route>
-					<Route path="/quiz-form">
-						<QuizForm />
-					</Route>
-				</Switch>
+				<main className={classes.rootMain}>
+					<Switch>
+						<Route exact path="/">
+							<Paper className={classes.container} square>
+								<Home />
+							</Paper>
+						</Route>
+						<Route path="/quiz-form">
+							<Paper className={classes.container} square>
+								<QuizForm />
+							</Paper>
+						</Route>
+					</Switch>
+				</main>
 			</Router>
 		</ThemeProvider>
 	);
